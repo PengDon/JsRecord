4698e983708af05fe5f93d95ab11184c60169d61
tangp <tangp@dtds.com.cn>
2018-09-05 17:29:08 +0800

    添加代码注释

--- src/duplicateRemoval.js
+++ src/duplicateRemoval.js
@@ -7,7 +7,7 @@
 const removeSome = (model,arr)=>{
     let newArr = []; 
     switch (model) {
-      case 1:
+      case 1: // 使用了ES6新增的set数据解构的去重特性，然后在将生成的set对象转换成数组d
         newArr = Array.from(new Set(arr));
         break;
     
