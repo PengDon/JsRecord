501af0f9d0c72386439d59186b0992fbf799cbfe
tangp <tangp@dtds.com.cn>
2018-09-05 17:27:52 +0800

    修改单元测试

--- test/exchange.test.js
+++ test/exchange.test.js
@@ -3,26 +3,26 @@ import exChange from '../src/exchange';
 let a = 33;
 let b = 99;
 
-test('model 1 to equal 99 33', () => {
+test(`model 1 to equal [${b},${a}]`, () => {
   expect(exChange(1,a,b)).toEqual([b,a]);
 })
 
-test('model 2 to equal 99 33', () => {
+test(`model 2 to equal [${b},${a}]`, () => {
   expect(exChange(2,a,b)).toEqual([b,a]);
 })
 
-test('model 3 to equal 99 33', () => {
+test(`model 3 to equal [${b},${a}]`, () => {
   expect(exChange(3,a,b)).toEqual([b,a]);
 })
 
-test('model 4 to equal 99 33', () => {
+test(`model 4 to equal [${b},${a}]`, () => {
   expect(exChange(4,a,b)).toEqual([b,a]);
 })
 
-test('model 5 to equal 99 33', () => {
+test(`model 5 to equal [${b},${a}]`, () => {
   expect(exChange(5,a,b)).toEqual([b,a]);
 })
 
-test('model 6 to equal 99 33', () => {
+test(`model 6 to equal [${b},${a}]`, () => {
   expect(exChange(6,a,b)).toEqual([b,a]);
 })
--- test/sort.test.js
+++ test/sort.test.js
@@ -3,7 +3,7 @@ import {bubbleSort} from '../src/sort';
 let nums = [1,43,65,33,2,5,6,8,112,4,21,63,30,9]; // 无序数组
 let rst = [1,2,4,5,6,8,9,21,30,33,43,63,65,112];  // 正序数组
 
-test('bubbleSort', () => {
+test(`bubbleSort toEqual ${rst}`, () => {
   expect(bubbleSort(nums)).toEqual(rst);
 });
 
